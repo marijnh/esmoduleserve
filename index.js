42428be88680556439945913850adcfe89fce77c
Marijn Haverbeke <marijn@haverbeke.nl>
2020-02-28 12:22:09 +0100

    First implementation of ES import rewriting

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-var ModuleServer = require("./moduleserver")
-var path = require("path")
-var send = require("send")
+const ModuleServer = require("./moduleserver")
+const path = require("path")
 
-var host = "localhost", port = 8080, dir = ".", transform = null, spaMode = false
+let host = "localhost", port = 8080, dir = ".", transform = null
 
 function usage() {
-  console.log("Usage: moduleserve [--port port] [--host host] [--transform module] [--spa] [dir]")
+  console.log("Usage: esmoduleserve [--port port] [--host host] [dir]")
   process.exit(1)
 }
 
 for (var i = 2; i < process.argv.length; i++) {
-  var arg = process.argv[i], next = process.argv[i + 1]
+  let arg = process.argv[i], next = process.argv[i + 1]
   if (arg == "--port" && next) { port = +next; i++ }
   else if (arg == "--host" && next) { host = next; i++ }
-  else if (arg == "--transform" && next) { transform = next; i++ }
-  else if (arg == "--spa") { spaMode = true; i++ }
   else if (dir == "." && arg[0] != "-") dir = arg
   else usage()
 }
 
 // The root directory being served.
-var root = path.resolve(dir)
+const root = path.resolve(dir)
 
-if (transform) {
-  var transformMod = require(transform == "babel" ? "./babel-transform" : path.resolve(transform))
-  if (transformMod.init) transformMod.init(root)
-  transform = transformMod.transform
-}
-
-var static = require("serve-static")(root)
-var moduleServer = new ModuleServer({root: root, transform: transform}).handleRequest
+const static = require("serve-static")(root)
+const moduleServer = new ModuleServer({root}).handleRequest
 
 // Create the server that listens to HTTP requests
 // and returns module contents.
-require("http").createServer(function(req, resp) {
+require("http").createServer((req, resp) => {
   if (moduleServer(req, resp)) return
   static(req, resp, function next(err) {
-    if (spaMode) {
-      send(req, path.join(root, "index.html")).pipe(resp);
-    } else {
-      resp.statusCode = 404
-      resp.end('Not found')
-    }
+    resp.statusCode = 404
+    resp.end('Not found')
   })
 }).listen(port, host)
 
--- moduleserver.js
+++ moduleserver.js
@@ -1,133 +1,113 @@
-var module_ = require("module"), pth = require("path"), fs = require("fs")
-var url_ = require("url")
+const pth = require("path"), fs = require("fs")
+const resolve = require("resolve")
+const {parse: parseURL} = require("url")
 
-function ModuleServer(options) {
-  this.root = unwin(options.root)
-  if (this.root.charAt(this.root.length - 1) != "/") this.root += "/"
-  this.transform = options.transform
-  this.cache = Object.create(null)
-  this.nextTag = 0
-  this.clientJS = fs.readFileSync(__dirname + "/client.js", "utf8")
-  // A map of module paths to the module's filesystem path.
-  this.resolved = Object.create(null)
+class Cached {
+  constructor(content, headers) {
+    this.content = content
+    this.headers = headers
+  }
+}
 
-  this.handleRequest = this.handleRequest.bind(this)
+class ModuleServer {
+  constructor(options) {
+    this.root = unwin(options.root)
+    if (this.root.charAt(this.root.length - 1) != "/") this.root += "/"
+    // Maps from paths (relative to root dir) to cache entries
+    this.cache = Object.create(null)
+    this.nextTag = 0
+    this.handleRequest = this.handleRequest.bind(this)
+  }
+
+  handleRequest(req, resp) {
+    let url = parseURL(req.url)
+    let handle = /^\/_m\/(.*)/.exec(url.pathname)
+    if (!handle) return false
+
+    let send = (status, text, headers) => {
+      let hds = {"access-control-allow-origin": "*",
+                 "x-request-url": req.url}
+      if (!headers || typeof headers == "string") hds["content-type"] = headers || "text/plain"
+      else Object.assign(hds, headers)
+      resp.writeHead(status, hds)
+      resp.end(text)
+    }
+
+    // Modules paths in URLs represent "up one directory" as "__".
+    // Convert them to ".." for filesystem path resolution.
+    let path = undash(handle[1])
+    let cached = this.cache[path]
+    if (!cached) {
+      let fullPath = unwin(pth.resolve(this.root, path)), code
+      try { code = fs.readFileSync(fullPath, "utf8") }
+      catch { send(404, "Not found"); return true }
+      let {code: resolvedCode, error} = this.resolveImports(fullPath, code)
+      if (error) { send(500, error); return true }
+      cached = this.cache[path] = new Cached(resolvedCode, {
+        "content-type": "application/javascript; charset=utf-8",
+        "etag": '"' + (++this.nextTag) + '"'
+      })
+    } else {
+      let noneMatch = req.headers["if-none-match"]
+      if (noneMatch && noneMatch.indexOf(cached.headers.etag) > -1) { send(304, null); return true }
+    }
+    send(200, cached.content, cached.headers)
+    return true
+  }
+
+  // Resolve a module path to a relative filepath where
+  // the module's file exists.
+  resolveModule(basePath, path) {
+    let resolved
+    try { resolved = resolve.sync(path, {basedir: basePath, packageFilter}) }
+    catch(e) { return {error: e.toString()} }
+
+    // Builtin modules resolve to strings like "fs". Try again with
+    // slash which makes it possible to locally install an equivalent.
+    if (resolved.indexOf("/") == -1) {
+      try { resolved = resolve.sync(path + "/", {basedir: basePath, packageFilter}) }
+      catch(e) { return {error: e.toString()} }
+    }
+
+    // FIXME deny relative paths that go up too much
+    let relative = unwin(pth.relative(basePath, resolved))
+    if (!/^\.\.?\//.test(relative)) relative = "./" + relative
+    return {path: relative}
+  }
+
+  resolveImports(basePath, code) {
+    ImportPattern.lastIndex = 0
+    let m, result = "", pos = 0
+    while (m = ImportPattern.exec(code)) {
+      let end = m.index + m[0].length, start = end - m[1].length, source = JSON.parse(m[1])
+      result += code.slice(pos, start)
+      let {error, path} = this.resolveModule(pth.dirname(basePath), source)
+      if (error) return {error}
+      result += JSON.stringify(dash(path))
+      pos = end
+    }
+    result += code.slice(pos)
+    return {code: result}
+  }
 }
 module.exports = ModuleServer
 
-ModuleServer.prototype.handleRequest = function(req, resp) {
-  var url = url_.parse(req.url)
-  var handle = /^\/moduleserve\/(?:load\.js$|mod\/(.*))/.exec(url.pathname)
-  if (!handle) return false
+const String = /'(?:[^\\']|\\.)*'|"(?:[^\\"]|\\.)*"/.source
+const Braces = /\{[^}]*\}/.source
+const S = /(?:\s|\/\/.*|\/\*.*?\*\/)*/.source
+const Id = /[\w$]+/.source
+const ImportPattern = new RegExp(
+  `\\b(?:import${S}(?:${Id}${S},${S})?(?:(?:${Braces}|${Id}|\\*${S}as${S}${Id})${S}from${S})?|` +
+       `export${S}${Braces}${S}from${S})(${String})`,
+  "g")
 
-  var send = function(status, text, headers) {
-    var hds = {"access-control-allow-origin": "*",
-               "x-request-url": req.url}
-    if (!headers || typeof headers == "string")
-      hds["content-type"] = headers || "text/plain"
-    else
-      for (var prop in headers) hds[prop] = headers[prop]
-    resp.writeHead(status, hds)
-    resp.end(text)
-  }
-
-  // matched /moduleserve/load.js
-  if (!handle[1]) {
-    send(200, this.clientJS, "application/javascript")
-    return true
-  }
-
-  // Modules paths in URLs represent "up one directory" as "__".
-  // Convert them to ".." for filesystem path resolution.
-  var path = undash(handle[1])
-  var found = this.resolved[path]
-  if (!found) {
-    found = this.resolveModule(path) || this.resolveModule(path + ".js")
-    if (!found) { send(404, "Not found"); return true }
-    this.resolved[path] = found
-  }
-
-  if (found != path) {
-    if (!(found in this.resolved)) this.resolved[found] = found
-    if (found != path + ".js") {
-      send(301, null, {location: "/moduleserve/mod/" + dash(found)})
-      return true
-    }
-  } else if (/\.js$/.test(path) && found.indexOf(/[^\/]+\.js$/.exec(path)[0] + "/") > -1) {
-    send(301, null, {location: "/moduleserve/mod/" + dash(found.slice(0, found.length - 3))})
-    return true
-  }
-
-  var cached = this.cache[found]
-  if (cached) {
-    var noneMatch = req.headers["if-none-match"]
-    if (noneMatch && noneMatch.indexOf(cached.headers.etag) > -1) send(304, null)
-    else send(200, cached.content, cached.headers)
-  } else {
-    this.sendScript(found, send)
-  }
-  return true
-}
-
-function undash(path) { return path.replace(/(^|\/)__(?=$|\/)/g, "$1..") }
 function dash(path) { return path.replace(/(^|\/)\.\.(?=$|\/)/g, "$1__") }
+function undash(path) { return path.replace(/(^|\/)__(?=$|\/)/g, "$1..") }
 
-function resolve() { return unwin(pth.resolve.apply(pth, arguments)) }
-function relative() { return unwin(pth.relative.apply(pth, arguments)) }
-var unwin = pth.sep == "\\" ? function(s) { return s.replace(/\\/g, "/") } : function(s) { return s }
+const unwin = pth.sep == "\\" ? s => s.replace(/\\/g, "/") : s => s
 
-// Resolve a module path to a relative filepath where
-// the module's file exists.
-ModuleServer.prototype.resolveModule = function(path) {
-  var localPath = resolve(this.root, path)
-  var hasMod = localPath.indexOf("/__mod/"), parent, modPath, resolved
-  if (hasMod > -1) {
-    parent = localPath.slice(0, hasMod)
-    modPath = localPath.slice(hasMod + 7)
-  } else {
-    parent = this.root
-    modPath = localPath
-  }
-
-  var dummyMod = {
-    id: parent,
-    paths: module_._nodeModulePaths(parent).concat(module_.globalPaths)
-  }
-  try { resolved = unwin(module_._resolveFilename(modPath, dummyMod)) }
-  catch(e) { return null }
-
-  // Handle builtin modules resolving to strings like "fs", try again
-  // with slash which makes it possible to locally install an equivalent.
-  if (resolved.indexOf("/") == -1) {
-    try { resolved = unwin(module_._resolveFilename(modPath + "/", dummyMod)) }
-    catch(e) { return null }
-  }
-
-  return relative(this.root, resolved)
-}
-
-function Cached(file, content, headers) {
-  this.file = file
-  this.content = content
-  this.headers = headers
-}
-
-// Send the script and cache the response in memory
-// for future requests for the same module.
-ModuleServer.prototype.sendScript = function(path, send) {
-  var content, localPath = resolve(this.root, path)
-  try { content = fs.readFileSync(localPath, "utf8") }
-  catch(e) { return send(404, "Not found") }
-  if (this.transform) content = this.transform(localPath, content)
-  var headers = {
-    "content-type": "application/javascript",
-    "etag": '"' + (++this.nextTag) + '"'
-  }
-  this.cache[path] = new Cached(localPath, content, headers)
-  // Bust the cache when the file changes.
-  var watching = fs.watch(localPath, function() {
-    watching.close()
-    cache[path] = null
-  }), cache = this.cache
-  return send(200, content, headers)
+function packageFilter(pkg) {
+  if (pkg.module) pkg.main = pkg.module
+  else if (pkg.jnext) pkg.main = pkg.jsnext
+  return pkg
 }
